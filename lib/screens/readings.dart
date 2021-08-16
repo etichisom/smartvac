0c48aa2fe8bfc3a790aa49989ed7a0788f022d20
ETI PETER <chisometi@gmail.com>
2021-08-16 03:10:31 +0100

    ok

---
+++ lib/screens/readings.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class Reading extends StatefulWidget {
+  @override
+  _ReadingState createState() => _ReadingState();
+}
+
+class _ReadingState extends State<Reading> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ListView(
+        children: [
+
+        ],
+      ),
+    );
+  }
+}
